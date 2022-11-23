9bd623c54e2674cd1d9f018cc19ef91b1e22b7fd
Julijan Katic <julijan@katic.io>
2022-11-23 14:45:36 +0100

    add contract

--- org.palladiosimulator.analyzer.slingshot.monitor/src/org/palladiosimulator/analyzer/slingshot/monitor/interpreter/MonitorRepositoryInterpreterBehavior.java
+++ org.palladiosimulator.analyzer.slingshot.monitor/src/org/palladiosimulator/analyzer/slingshot/monitor/interpreter/MonitorRepositoryInterpreterBehavior.java
@@ -5,9 +5,13 @@ import javax.inject.Inject;
 import org.palladiosimulator.analyzer.slingshot.core.events.PreSimulationConfigurationStarted;
 import org.palladiosimulator.analyzer.slingshot.core.extension.SimulationBehaviorExtension;
 import org.palladiosimulator.analyzer.slingshot.eventdriver.annotations.Subscribe;
+import org.palladiosimulator.analyzer.slingshot.eventdriver.annotations.eventcontract.EventCardinality;
+import org.palladiosimulator.analyzer.slingshot.eventdriver.annotations.eventcontract.OnEvent;
 import org.palladiosimulator.analyzer.slingshot.eventdriver.returntypes.Result;
+import org.palladiosimulator.analyzer.slingshot.monitor.data.events.modelvisited.MonitorModelVisited;
 import org.palladiosimulator.monitorrepository.MonitorRepository;
 
+@OnEvent(when = PreSimulationConfigurationStarted.class, then = { MonitorModelVisited.class }, cardinality = EventCardinality.MANY)
 public class MonitorRepositoryInterpreterBehavior implements SimulationBehaviorExtension {
 
 	private final MonitorRepository monitorRepository;
