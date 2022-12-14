b1eb0ec4f9e1d11d3f8e94b476b46855f42ab90a
Sarah Sophie Stieß <sarah.stiess@iste.uni-stuttgart.de>
2022-12-14 11:44:43 +0100

    fix multiple calculators bug

--- org.palladiosimulator.analyzer.slingshot.monitor/src/org/palladiosimulator/analyzer/slingshot/monitor/recorder/decorator/MonitoringAttachingCalculatorFactoryDecorator.java
+++ org.palladiosimulator.analyzer.slingshot.monitor/src/org/palladiosimulator/analyzer/slingshot/monitor/recorder/decorator/MonitoringAttachingCalculatorFactoryDecorator.java
@@ -2,9 +2,12 @@ package org.palladiosimulator.analyzer.slingshot.monitor.recorder.decorator;
 
 import javax.inject.Inject;
 
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.palladiosimulator.analyzer.slingshot.monitor.observer.EventBasedMeasurementObserver;
 import org.palladiosimulator.analyzer.slingshot.monitor.utils.SlingshotCalculatorWrapper;
 import org.palladiosimulator.edp2.models.measuringpoint.MeasuringPoint;
+import org.palladiosimulator.edp2.models.measuringpoint.MeasuringPointRepository;
+import org.palladiosimulator.edp2.models.measuringpoint.MeasuringpointFactory;
 import org.palladiosimulator.metricspec.MetricDescription;
 import org.palladiosimulator.probeframework.calculator.Calculator;
 import org.palladiosimulator.probeframework.calculator.CalculatorProbeSet;
@@ -14,7 +17,7 @@ public final class MonitoringAttachingCalculatorFactoryDecorator implements IGen
 
 	private final IGenericCalculatorFactory delegator;
 	private final EventBasedMeasurementObserver observer;
-	
+
 	@Inject
 	public MonitoringAttachingCalculatorFactoryDecorator(
 			final IGenericCalculatorFactory delegator,
@@ -22,12 +25,17 @@ public final class MonitoringAttachingCalculatorFactoryDecorator implements IGen
 		this.delegator = delegator;
 		this.observer = observer;
 	}
-	
+
 	@Override
-	public Calculator buildCalculator(MetricDescription arg0, MeasuringPoint arg1, CalculatorProbeSet arg2) {
-		return this.setupCalculator(this.delegator.buildCalculator(arg0, arg1, arg2));
+	public Calculator buildCalculator(final MetricDescription arg0, final MeasuringPoint arg1, final CalculatorProbeSet arg2) {
+		// create copy
+		final MeasuringPointRepository repo = MeasuringpointFactory.eINSTANCE.createMeasuringPointRepository();
+		final MeasuringPoint copy = EcoreUtil.copy(arg1);
+		copy.setMeasuringPointRepository(repo);
+
+		return this.setupCalculator(this.delegator.buildCalculator(arg0, copy, arg2));
 	}
-	
+
 	private Calculator setupCalculator(final Calculator calculator) {
 		calculator.addObserver(SlingshotCalculatorWrapper.wrap(calculator, observer));
 		return calculator;
