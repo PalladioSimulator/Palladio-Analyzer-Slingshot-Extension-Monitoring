440b202e9ae2554eaae1d99b2fed7c56e0f82340
Sarah Sophie Stieß <sarah.stiess@iste.uni-stuttgart.de>
2023-03-17 12:58:10 +0100

    switch Provider form javax to google. no clue why that fixes my type issues.

--- org.palladiosimulator.analyzer.slingshot.monitor/src/org/palladiosimulator/analyzer/slingshot/monitor/calculator/CalculatorFactoryProvider.java
+++ org.palladiosimulator.analyzer.slingshot.monitor/src/org/palladiosimulator/analyzer/slingshot/monitor/calculator/CalculatorFactoryProvider.java
@@ -3,7 +3,6 @@ package org.palladiosimulator.analyzer.slingshot.monitor.calculator;
 import de.uka.ipd.sdq.simucomframework.SimuComConfig;
 
 import javax.inject.Inject;
-import javax.inject.Provider;
 import javax.inject.Singleton;
 
 import org.palladiosimulator.analyzer.slingshot.monitor.observer.EventBasedMeasurementObserver;
@@ -12,24 +11,26 @@ import org.palladiosimulator.analyzer.slingshot.monitor.recorder.decorator.Recor
 import org.palladiosimulator.probeframework.calculator.ExtensibleCalculatorFactoryDelegatingFactory;
 import org.palladiosimulator.probeframework.calculator.IGenericCalculatorFactory;
 
+import com.google.inject.Provider;
+
 @Singleton
 public class CalculatorFactoryProvider implements Provider<IGenericCalculatorFactory> {
 
 	private final SimuComConfig simuComConfig;
 	private final EventBasedMeasurementObserver observer;
-	
+
 	@Inject
 	public CalculatorFactoryProvider(final SimuComConfig simuComConfig, final EventBasedMeasurementObserver observer) {
 		this.simuComConfig = simuComConfig;
 		this.observer = observer;
 	}
-	
+
 	@Override
 	public IGenericCalculatorFactory get() {
 		final IGenericCalculatorFactory parentFactory = new ExtensibleCalculatorFactoryDelegatingFactory();
 		final IGenericCalculatorFactory recorderAttachedFactory = new RecorderAttachingCalculatorFactoryDecorator(parentFactory, simuComConfig.getRecorderName(), simuComConfig.getRecorderConfigurationFactory());
 		final IGenericCalculatorFactory monitorAttachedFactory = new MonitoringAttachingCalculatorFactoryDecorator(recorderAttachedFactory, this.observer);
-		
+
 		return monitorAttachedFactory;
 	}
 }
--- org.palladiosimulator.analyzer.slingshot.monitor/src/org/palladiosimulator/analyzer/slingshot/monitor/probes/ProbeFrameworkContextProvider.java
+++ org.palladiosimulator.analyzer.slingshot.monitor/src/org/palladiosimulator/analyzer/slingshot/monitor/probes/ProbeFrameworkContextProvider.java
@@ -1,22 +1,23 @@
 package org.palladiosimulator.analyzer.slingshot.monitor.probes;
 
 import javax.inject.Inject;
-import javax.inject.Provider;
 import javax.inject.Singleton;
 
 import org.palladiosimulator.probeframework.ProbeFrameworkContext;
 import org.palladiosimulator.probeframework.calculator.IGenericCalculatorFactory;
 
+import com.google.inject.Provider;
+
 @Singleton
 public class ProbeFrameworkContextProvider implements Provider<ProbeFrameworkContext> {
 
 	private final IGenericCalculatorFactory calculatorFactory;
-	
+
 	@Inject
 	public ProbeFrameworkContextProvider(final IGenericCalculatorFactory calculatorFactory) {
 		this.calculatorFactory = calculatorFactory;
 	}
-	
+
 	@Override
 	public ProbeFrameworkContext get() {
 		return new ProbeFrameworkContext(this.calculatorFactory);
