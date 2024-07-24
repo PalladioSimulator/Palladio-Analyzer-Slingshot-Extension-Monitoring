0c378fdf1f51224b1ef20e5aa920fc92e18eced3
Floriment Klinaku <klinakuf@gmail.com>
2024-07-24 15:32:59 +0200

    dependency from monitoring to spd is removed

--- bundles/org.palladiosimulator.analyzer.slingshot.monitor/src/org/palladiosimulator/analyzer/slingshot/monitor/interpreter/MonitorRepositoryInterpreterBehavior.java
+++ bundles/org.palladiosimulator.analyzer.slingshot.monitor/src/org/palladiosimulator/analyzer/slingshot/monitor/interpreter/MonitorRepositoryInterpreterBehavior.java
@@ -1,13 +1,13 @@
 package org.palladiosimulator.analyzer.slingshot.monitor.interpreter;
 
 import java.util.Set;
-import java.util.stream.Collectors;
+//import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 
 import org.eclipse.emf.ecore.EObject;
-import org.palladiosimulator.analyzer.slingshot.behavior.spd.data.ModelAdjusted;
-import org.palladiosimulator.analyzer.slingshot.behavior.spd.data.adjustment.MonitorChange;
+//import org.palladiosimulator.analyzer.slingshot.behavior.spd.data.ModelAdjusted;
+//import org.palladiosimulator.analyzer.slingshot.behavior.spd.data.adjustment.MonitorChange;
 import org.palladiosimulator.analyzer.slingshot.common.annotations.Nullable;
 import org.palladiosimulator.analyzer.slingshot.core.events.PreSimulationConfigurationStarted;
 import org.palladiosimulator.analyzer.slingshot.core.extension.SimulationBehaviorExtension;
@@ -20,7 +20,7 @@ import org.palladiosimulator.analyzer.slingshot.monitor.data.events.modelvisited
 import org.palladiosimulator.monitorrepository.MonitorRepository;
 
 @OnEvent(when = PreSimulationConfigurationStarted.class, then = { MonitorModelVisited.class }, cardinality = EventCardinality.MANY)
-@OnEvent(when = ModelAdjusted.class, then = { MonitorModelVisited.class }, cardinality = EventCardinality.MANY)
+//@OnEvent(when = ModelAdjusted.class, then = { MonitorModelVisited.class }, cardinality = EventCardinality.MANY)
 public class MonitorRepositoryInterpreterBehavior implements SimulationBehaviorExtension {
 
 	private final MonitorRepository monitorRepository;
@@ -40,16 +40,16 @@ public class MonitorRepositoryInterpreterBehavior implements SimulationBehaviorE
 		return Result.of(this.interpreteMonitorModel(monitorRepository));
 	}
 
-	@Subscribe
-	public Result<MonitoringEvent> onModelAdjusted(final ModelAdjusted modelAdjusted) {
-		final Set<MonitoringEvent> result =	modelAdjusted.getChanges().stream()
-				.filter(MonitorChange.class::isInstance)
-				.map(MonitorChange.class::cast)
-				.flatMap(monitorChange -> this.interpreteMonitorModel(monitorChange.getNewMonitor()).stream())
-				.collect(Collectors.toSet());
-
-		return Result.of(result);
-	}
+//	@Subscribe
+//	public Result<MonitoringEvent> onModelAdjusted(final ModelAdjusted modelAdjusted) {
+//		final Set<MonitoringEvent> result =	modelAdjusted.getChanges().stream()
+//				.filter(MonitorChange.class::isInstance)
+//				.map(MonitorChange.class::cast)
+//				.flatMap(monitorChange -> this.interpreteMonitorModel(monitorChange.getNewMonitor()).stream())
+//				.collect(Collectors.toSet());
+//
+//		return Result.of(result);
+//	}
 
 	private Set<MonitoringEvent> interpreteMonitorModel(final EObject modelElement) {
 		final MonitorModelVisitor visitor = new MonitorModelVisitor();
